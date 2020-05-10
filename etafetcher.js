811a3de84b5363fd42afae9c67f334f2aaf62a03
root <winstonma@ymail.com>
2020-05-10 04:41:49 +0000

    Convert tab with spaces

--- etafetcher.js
+++ etafetcher.js
@@ -19,120 +19,120 @@ const etaUrl = "http://etav3.kmb.hk/?"
  */
 
 var ETAFetcher = function (stopInfo, reloadInterval) {
-	var self = this;
-	reloadInterval = (reloadInterval < 1000) ? 1000 : reloadInterval;
+  var self = this;
+  reloadInterval = (reloadInterval < 1000) ? 1000 : reloadInterval;
 
-	var reloadTimer = null;
-	var items = [];
+  var reloadTimer = null;
+  var items = [];
 
-	var fetchFailedCallback = function () { };
-	var itemsReceivedCallback = function () { };
+  var fetchFailedCallback = function () { };
+  var itemsReceivedCallback = function () { };
 
-	// Generate Url
-	const parseQueryString = {
-		action: "geteta",
-		lang: "tc",
-		route: stopInfo.Route,
-		bound: stopInfo.Bound,
-		stop: stopInfo.BSICode,
-		stop_seq: stopInfo.Seq
-	};
-	const url = etaUrl + querystring.stringify(parseQueryString);
+  // Generate Url
+  const parseQueryString = {
+    action: "geteta",
+    lang: "tc",
+    route: stopInfo.Route,
+    bound: stopInfo.Bound,
+    stop: stopInfo.BSICode,
+    stop_seq: stopInfo.Seq
+  };
+  const url = etaUrl + querystring.stringify(parseQueryString);
 
-	/* private methods */
+  /* private methods */
 
 	/* fetchETA()
 	 * Request the ETA.
 	 */
-	var fetchETA = function () {
-		clearTimeout(reloadTimer);
-		reloadTimer = null;
-		items = [];
+  var fetchETA = function () {
+    clearTimeout(reloadTimer);
+    reloadTimer = null;
+    items = [];
 
-		request(url, (error, response, body) => {
-			if (response.statusCode === 200) {
-				responseObj = JSON.parse(body);
-				if (!responseObj || responseObj.responsecode != 0) {
-					console.log("Error obtaining ETA connections " + response.statusCode);
-					fetchFailedCallback(self, error);
-					scheduleTimer();
-				}
-				//responseObj.url = url;
-				responseObj.stopInfo = stopInfo;
-				items.push(responseObj);
-				self.broadcastItems();
-				scheduleTimer();
-			} else {
-				console.log("Error getting ETA connections " + response.statusCode);
-				fetchFailedCallback(self, error);
-				scheduleTimer();
-			}
-		});
+    request(url, (error, response, body) => {
+      if (response.statusCode === 200) {
+        responseObj = JSON.parse(body);
+        if (!responseObj || responseObj.responsecode != 0) {
+          console.log("Error obtaining ETA connections " + response.statusCode);
+          fetchFailedCallback(self, error);
+          scheduleTimer();
+        }
+        //responseObj.url = url;
+        responseObj.stopInfo = stopInfo;
+        items.push(responseObj);
+        self.broadcastItems();
+        scheduleTimer();
+      } else {
+        console.log("Error getting ETA connections " + response.statusCode);
+        fetchFailedCallback(self, error);
+        scheduleTimer();
+      }
+    });
 
-	};
+  };
 
 	/* scheduleTimer()
 	 * Schedule the timer for the next update.
 	 */
-	var scheduleTimer = function () {
-		//console.log('Schedule update timer.');
-		clearTimeout(reloadTimer);
-		reloadTimer = setTimeout(function () {
-			fetchETA();
-		}, reloadInterval);
-	};
+  var scheduleTimer = function () {
+    //console.log('Schedule update timer.');
+    clearTimeout(reloadTimer);
+    reloadTimer = setTimeout(function () {
+      fetchETA();
+    }, reloadInterval);
+  };
 
-	/* public methods */
+  /* public methods */
 
 	/* setReloadInterval()
 	 * Update the reload interval, but only if we need to increase the speed.
 	 *
 	 * attribute interval number - Interval for the update in milliseconds.
 	 */
-	this.setReloadInterval = function (interval) {
-		if (interval > 1000 && interval < reloadInterval) {
-			reloadInterval = interval;
-		}
-	};
+  this.setReloadInterval = function (interval) {
+    if (interval > 1000 && interval < reloadInterval) {
+      reloadInterval = interval;
+    }
+  };
 
 	/* startFetch()
 	 * Initiate fetchETA();
 	 */
-	this.startFetch = function () {
-		fetchETA();
-	};
+  this.startFetch = function () {
+    fetchETA();
+  };
 
 	/* broadcastItems()
 	 * Broadcast the existing items.
 	 */
-	this.broadcastItems = function () {
-		if (items.length <= 0) {
-			//console.log('No items to broadcast yet.');
-			return;
-		}
-		//console.log('Broadcasting ' + items.length + ' items.');
-		itemsReceivedCallback(self);
-	};
+  this.broadcastItems = function () {
+    if (items.length <= 0) {
+      //console.log('No items to broadcast yet.');
+      return;
+    }
+    //console.log('Broadcasting ' + items.length + ' items.');
+    itemsReceivedCallback(self);
+  };
 
-	this.onReceive = function (callback) {
-		itemsReceivedCallback = callback;
-	};
+  this.onReceive = function (callback) {
+    itemsReceivedCallback = callback;
+  };
 
-	this.onError = function (callback) {
-		fetchFailedCallback = callback;
-	};
+  this.onError = function (callback) {
+    fetchFailedCallback = callback;
+  };
 
-	this.url = function () {
-		return url;
-	};
+  this.url = function () {
+    return url;
+  };
 
-	this.route = function () {
-		return stopInfo.Route;
-	}
+  this.route = function () {
+    return stopInfo.Route;
+  }
 
-	this.items = function () {
-		return items[0];
-	};
+  this.items = function () {
+    return items[0];
+  };
 };
 
 module.exports = ETAFetcher;
--- node_helper.js
+++ node_helper.js
@@ -13,163 +13,163 @@ const BusRouteFetcher = require("./busroutefetcher.js");
 
 module.exports = NodeHelper.create({
 
-    start: function () {
-        console.log("Starting node helper for: " + this.name);
-        // Fetchers for all stops
-        this.stopFetchers = [];
-        // Fetchers for all ETAs
-        this.etaFetchers = [];
-        // Stores the lookup of Stop ID and Stop Name
-        this.stopName = [];
-    },
+  start: function () {
+    console.log("Starting node helper for: " + this.name);
+    // Fetchers for all stops
+    this.stopFetchers = [];
+    // Fetchers for all ETAs
+    this.etaFetchers = [];
+    // Stores the lookup of Stop ID and Stop Name
+    this.stopName = [];
+  },
 
-    socketNotificationReceived: function (notification, payload) {
-        // Request for information
-        if (notification === "ADD_STOPS") {
-            for (var f in payload.config) {
-                var stopID = payload.config[f].stopID;
-                // Create a new entry with no stop name
-                this.stopName[stopID] = '';
-                this.getStopInfo(stopID);
-            }
-            return;
+  socketNotificationReceived: function (notification, payload) {
+    // Request for information
+    if (notification === "ADD_STOPS") {
+      for (var f in payload.config) {
+        var stopID = payload.config[f].stopID;
+        // Create a new entry with no stop name
+        this.stopName[stopID] = '';
+        this.getStopInfo(stopID);
+      }
+      return;
+    }
+  },
+
+  getData: function (options, stopID) {
+    request(options, (error, response, body) => {
+      if (response.statusCode === 200) {
+        this.sendSocketNotification("KMBETA" + stopID, JSON.parse(body));
+      } else {
+        console.log("Error getting tram connections " + response.statusCode);
+      }
+    });
+  },
+
+  /*
+   * Obtain the route that pass through this bus stop
+   * @param {stopID} the stop ID of the bus stop
+   */
+  getStopInfo: function (stopID) {
+    var self = this;
+    var reloadInterval = config.reloadInterval || 5 * 60 * 1000;
+    var fetcher;
+    if (typeof self.stopFetchers[stopID] === "undefined") {
+      console.log("Create new stop fetcher for stopID: " + stopID + " - Interval: " + reloadInterval);
+      fetcher = new BusStopFetcher(stopID, reloadInterval);
+      fetcher.onReceive(function (fetcher) {
+        const items = fetcher.items();
+        const stopID = fetcher.stopID();
+        for (var f in items) {
+          var routes = items[f].data;
+          routes.map((route) => self.createRouteFetcher(route.trim(), stopID));
         }
-    },
-
-    getData: function (options, stopID) {
-        request(options, (error, response, body) => {
-            if (response.statusCode === 200) {
-                this.sendSocketNotification("KMBETA" + stopID, JSON.parse(body));
-            } else {
-                console.log("Error getting tram connections " + response.statusCode);
-            }
+      });
+      fetcher.onError(function (fetcher, error) {
+        self.sendSocketNotification("FETCH_ERROR", {
+          url: fetcher.url(),
+          error: error
         });
-    },
+      });
+      self.stopFetchers[stopID] = fetcher;
+    } else {
+      console.log("Use existing Stop fetcher for stopID: " + stopID);
+      fetcher = self.stopFetchers[stopID];
+      fetcher.setReloadInterval(reloadInterval);
+      fetcher.broadcastItems();
+    }
+    fetcher.startFetch();
+  },
 
-    /*
-     * Obtain the route that pass through this bus stop
-     * @param {stopID} the stop ID of the bus stop
-     */
-    getStopInfo: function (stopID) {
-        var self = this;
-        var reloadInterval = config.reloadInterval || 5 * 60 * 1000;
-        var fetcher;
-        if (typeof self.stopFetchers[stopID] === "undefined") {
-            console.log("Create new stop fetcher for stopID: " + stopID + " - Interval: " + reloadInterval);
-            fetcher = new BusStopFetcher(stopID, reloadInterval);
-            fetcher.onReceive(function (fetcher) {
-                const items = fetcher.items();
-                const stopID = fetcher.stopID();
-                for (var f in items) {
-                    var routes = items[f].data;
-                    routes.map((route) => self.createRouteFetcher(route.trim(), stopID));
-                }
-            });
-            fetcher.onError(function (fetcher, error) {
-                self.sendSocketNotification("FETCH_ERROR", {
-                    url: fetcher.url(),
-                    error: error
-                });
-            });
-            self.stopFetchers[stopID] = fetcher;
-        } else {
-            console.log("Use existing Stop fetcher for stopID: " + stopID);
-            fetcher = self.stopFetchers[stopID];
-            fetcher.setReloadInterval(reloadInterval);
-            fetcher.broadcastItems();
-        }
-        fetcher.startFetch();
-    },
+  /* Creates a fetcher for collecting ETA info
+   *
+   * @param {stopInfo} the stop info (an object)
+   */
+  createETAFetcher: function (stopInfo) {
+    var self = this;
 
-    /* Creates a fetcher for collecting ETA info
-     *
-     * @param {stopInfo} the stop info (an object)
-     */
-    createETAFetcher: function (stopInfo) {
-        var self = this;
+    const reloadInterval = 60 * 1000;
+    var fetcher = new ETAFetcher(stopInfo, reloadInterval);
 
-        const reloadInterval = 60 * 1000;
-        var fetcher = new ETAFetcher(stopInfo, reloadInterval);
+    const url = fetcher.url();
+    if (!validUrl.isUri(url)) {
+      self.sendSocketNotification("INCORRECT_URL", url);
+      return;
+    }
 
-        const url = fetcher.url();
-        if (!validUrl.isUri(url)) {
-            self.sendSocketNotification("INCORRECT_URL", url);
-            return;
-        }
-
-        if (typeof self.etaFetchers[url] === "undefined") {
-            console.log("Create new ETA fetcher for url: " + url + " - Interval: " + reloadInterval);
-            fetcher.onReceive(function (fetcher) {
-                self.broadcastETAs();
-            });
-            fetcher.onError(function (fetcher, error) {
-                self.sendSocketNotification("FETCH_ERROR", {
-                    url: fetcher.url(),
-                    error: error
-                });
-            });
-            self.etaFetchers[url] = fetcher;
-        } else {
-            console.log("Use existing ETA fetcher for url: " + url);
-            fetcher = self.etaFetchers[url];
-            fetcher.broadcastItems();
-        }
-        fetcher.startFetch();
-    },
-
-    /* createRouteFetcher(route, stopID)
-     * Creates a fetcher for a new url if it doesn't exist yet.
-     *
-     * attribute url string - URL of the news feed.
-     * attribute busStop number - BusStopID
-     */
-    createRouteFetcher: function (route, stopID) {
-        var self = this;
-        var fetcher;
-        fetcher = new BusRouteFetcher(route, stopID);
-        fetcher.onReceive(function (fetcher) {
-            // Keep relevant bounds and find the stop name
-            fetcher.items().filter((routeInfo) => {
-                // Check if the stop is in the routeinfo
-                const match = routeInfo.routeStops.filter(stops =>
-                    stops.BSICode.split("-")[0] === stopID.split("-")[0] &&
-                    stops.BSICode.split("-")[1] === stopID.split("-")[1]
-                );
-                // Find out the stop name
-                if (self.stopName[stopID] === '') {
-                    const match = routeInfo.routeStops.filter(stops => stops.BSICode === stopID);
-                    if (match.length == 1) {
-                        // Find the exact match, store the stop name
-                        self.stopName[stopID] = match[0].CName;
-                    }
-                }
-                if (match.length == 1) {
-                    // Find the desired stop, obtain the ETA
-                    match[0].basicInfo = routeInfo.basicInfo;
-                    self.createETAFetcher(match[0]);
-                }
-            });
+    if (typeof self.etaFetchers[url] === "undefined") {
+      console.log("Create new ETA fetcher for url: " + url + " - Interval: " + reloadInterval);
+      fetcher.onReceive(function (fetcher) {
+        self.broadcastETAs();
+      });
+      fetcher.onError(function (fetcher, error) {
+        self.sendSocketNotification("FETCH_ERROR", {
+          url: fetcher.url(),
+          error: error
         });
-        fetcher.onError(function (fetcher, error) {
-            self.sendSocketNotification("FETCH_ERROR", {
-                url: fetcher.url(),
-                error: error
-            });
-        });
-        fetcher.startFetch();
-    },
+      });
+      self.etaFetchers[url] = fetcher;
+    } else {
+      console.log("Use existing ETA fetcher for url: " + url);
+      fetcher = self.etaFetchers[url];
+      fetcher.broadcastItems();
+    }
+    fetcher.startFetch();
+  },
 
-    /* broadcastETAs()
-     * Creates an object with all feed items of the different registered ETAs,
-     * and broadcasts these using sendSocketNotification.
-     */
-    broadcastETAs: function () {
-        let etas = [];
-        for (const [url, fetcher] of Object.entries(this.etaFetchers)) {
-            if (fetcher.items() != null) {
-                etas.push(fetcher.items());
-            }
+  /* createRouteFetcher(route, stopID)
+   * Creates a fetcher for a new url if it doesn't exist yet.
+   *
+   * attribute url string - URL of the news feed.
+   * attribute busStop number - BusStopID
+   */
+  createRouteFetcher: function (route, stopID) {
+    var self = this;
+    var fetcher;
+    fetcher = new BusRouteFetcher(route, stopID);
+    fetcher.onReceive(function (fetcher) {
+      // Keep relevant bounds and find the stop name
+      fetcher.items().filter((routeInfo) => {
+        // Check if the stop is in the routeinfo
+        const match = routeInfo.routeStops.filter(stops =>
+          stops.BSICode.split("-")[0] === stopID.split("-")[0] &&
+          stops.BSICode.split("-")[1] === stopID.split("-")[1]
+        );
+        // Find out the stop name
+        if (self.stopName[stopID] === '') {
+          const match = routeInfo.routeStops.filter(stops => stops.BSICode === stopID);
+          if (match.length == 1) {
+            // Find the exact match, store the stop name
+            self.stopName[stopID] = match[0].CName;
+          }
         }
-        this.sendSocketNotification("ETA_ITEMS", etas);
-    },
+        if (match.length == 1) {
+          // Find the desired stop, obtain the ETA
+          match[0].basicInfo = routeInfo.basicInfo;
+          self.createETAFetcher(match[0]);
+        }
+      });
+    });
+    fetcher.onError(function (fetcher, error) {
+      self.sendSocketNotification("FETCH_ERROR", {
+        url: fetcher.url(),
+        error: error
+      });
+    });
+    fetcher.startFetch();
+  },
+
+  /* broadcastETAs()
+   * Creates an object with all feed items of the different registered ETAs,
+   * and broadcasts these using sendSocketNotification.
+   */
+  broadcastETAs: function () {
+    let etas = [];
+    for (const [url, fetcher] of Object.entries(this.etaFetchers)) {
+      if (fetcher.items() != null) {
+        etas.push(fetcher.items());
+      }
+    }
+    this.sendSocketNotification("ETA_ITEMS", etas);
+  },
 });
