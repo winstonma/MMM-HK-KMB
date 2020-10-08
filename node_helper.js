9734fa632f9ea068045f0c7dd3bdc44f1b1d1632
root <winstonma@ymail.com>
2020-10-08 05:21:32 +0000

    Cleanup node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,8 +1,8 @@
 /* Magic Mirror
- * Module: MMM-HK-Transport
+ * Module: MMM-HK-KMB
  *
  * By Winston / https://github.com/winstonma
- * MIT Licensed.
+ * AGPL-3.0 Licensed.
  */
 
 const NodeHelper = require("node_helper");
@@ -11,34 +11,33 @@ const BusStopFetcher = require("./busstopfetcher.js");
 const Log = require("../../js/logger");
 
 module.exports = NodeHelper.create({
-
+  // Override start method.
   start: function () {
     Log.log("Starting node helper for: " + this.name);
     // Fetchers for all stops
     this.stopFetchers = [];
   },
 
-  socketNotificationReceived: function (notification, payload) {
-    // Request for information
+    // Override socketNotificationReceived received.
+    socketNotificationReceived: function (notification, payload) {
     if (notification === "ADD_STOP") {
       this.getStopInfo(payload.stop.stopID, payload.config);
     }
   },
 
-  /* getStopInfo(stopID, config)
-   * Obtain the route that pass through this bus stop
+  /**
+   * Obtain the route pass through this bus stop
    *
-   * attribute stopID string - The stopID of the bus stop
-   * attribute config object - A configuration object containing reload interval in milliseconds.
+   * @param {object} stopID The stop ID.
+   * @param {object} config The configuration object.
    */
   getStopInfo: function (stopID, config) {
-    const self = this;
     let fetcher;
 
-    if (typeof self.stopFetchers[stopID] === "undefined") {
-      Log.log("Create new stop fetcher for stopID: " + stopID);
+    if (typeof this.stopFetchers[stopID] === "undefined") {
+      Log.log(`Create new stop fetcher for stopID: ${stopID}`);
       fetcher = new BusStopFetcher(stopID);
-      fetcher.onReceive(function (fetcher) {
+      fetcher.onReceive((fetcher) => {
         // Use the sorting function to arrange the bus route within the stop
         const stopInfoSorted = Object.entries(fetcher.item())
           .sort(([, [a]], [, [b]]) => {
@@ -57,70 +56,73 @@ module.exports = NodeHelper.create({
 
         const stopName = Object.values(stopInfoSorted).find(v => v[0].stop.id == stopID)[0].stop.name;
 
-        self.sendSocketNotification("STOP_ITEM", {
+        this.sendSocketNotification("STOP_ITEM", {
           stopID: stopID,
           stopName: stopName,
           stopInfo: stopInfoSorted
         });
 
         // Remove all exising ETA fetchers, if stop info is being updated
-        if (self.stopFetchers[stopID].etaFetchers)
-          self.stopFetchers[stopID].etaFetchers = {};
+        if (this.stopFetchers[stopID].etaFetchers)
+          this.stopFetchers[stopID].etaFetchers = {};
 
         // Fetch the ETA for the stop
         Object.values(stopInfoSorted).forEach(stops => {
           stops.forEach(stop => {
-            self.createETAFetcher(stopID, stop);
+            this.createETAFetcher(stopID, stop, config);
           });
         });
       });
-      fetcher.onError(function (fetcher, error) {
-        self.sendSocketNotification("FETCH_ERROR", {
+
+      fetcher.onError((fetcher, error) => {
+        this.sendSocketNotification("FETCH_ERROR", {
           stopID: fetcher.stopID(),
           error: error
         });
       });
-      self.stopFetchers[stopID] = {
+      this.stopFetchers[stopID] = {
         Fetcher: fetcher,
         etaFetchers: {}
       };
     } else {
-      Log.log("Use existing Stop fetcher for stopID: " + stopID);
-      fetcher = self.stopFetchers[stopID]["Fetcher"];
+      Log.log(`Use existing Stop fetcher for stopID: ${stopID}`);
+      fetcher = this.stopFetchers[stopID]["Fetcher"];
       fetcher.broadcastItems();
     }
     fetcher.startFetch();
   },
 
-  /* Creates a fetcher for collecting ETA info
+  /**
+   * Creates a fetcher for collecting ETA info
    *
    * @param {stopID} the stop ID (a string)
    * @param {stop} the stop info (an object)
+   * @param {config} the config parameter (an object)
    */
-  createETAFetcher: function (stopID, stop) {
-    const self = this;
-
-    const reloadInterval = 60 * 1000;
+  createETAFetcher: function (stopID, stop, config) {
+    const reloadInterval = config.reloadInterval || 5 * 60 * 1000;
     let fetcher = new ETAFetcher(stop, reloadInterval);
 
     const fetcherKey = JSON.stringify(stop);
 
-    if (!Object.keys(self.stopFetchers[stopID]["etaFetchers"]).includes(fetcherKey)) {
-      Log.log("Create new ETA fetcher for route: " + stop.variant.route.number + " - Interval: " + reloadInterval);
-      fetcher.onReceive(function (fetcher) {
-        self.broadcastETAs();
+    if (!Object.keys(this.stopFetchers[stopID]["etaFetchers"]).includes(fetcherKey)) {
+      Log.log(`Create new ETA fetcher for route: ${stop.variant.route.number} - Interval: ${reloadInterval}`);
+
+      fetcher.onReceive(() => {
+        this.broadcastETAs();
       });
-      fetcher.onError(function (fetcher, error) {
-        self.sendSocketNotification("FETCH_ERROR", {
-          url: fetcher.url(),
+
+      fetcher.onError((fetcher, error) => {
+        this.sendSocketNotification("FETCH_ERROR", {
+          url: stop.route,
           error: error
         });
       });
 
-      self.stopFetchers[stopID]["etaFetchers"][fetcherKey] = fetcher;
+      this.stopFetchers[stopID]["etaFetchers"][fetcherKey] = fetcher;
     } else {
-      Log.log("Use existing ETA fetcher for route: " + stop.variant.route.number);
-      fetcher = self.stopFetchers[stopID]["etaFetchers"][fetcherKey];
+      Log.log(`Use existing ETA fetcher for route: ${stop.variant.route.number}`);
+      fetcher = this.stopFetchers[stopID]["etaFetchers"][fetcherKey];
       fetcher.broadcastItems();
     }
     fetcher.startFetch();
