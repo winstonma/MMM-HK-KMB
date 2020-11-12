f0ed513d82ed0e5e902e788042cac136be35d383
root <winstonma@ymail.com>
2020-11-12 05:27:43 +0000

    Avoid missing route

--- busstopfetcher.js
+++ busstopfetcher.js
@@ -27,7 +27,7 @@ const BusStopFetcher = function (stopID) {
   }
   const lang = langTable[config.language] || 'en';
   const kmb = new Kmb(lang);
-  const stop = stopID ? new kmb.Stop(stopID, null, null) : null;
+  let stopName = '';
 
   // Create a schduler to update the bustop info (every 5am)
   cron.schedule('* 5 * * *', () => {
@@ -45,13 +45,21 @@ const BusStopFetcher = function (stopID) {
   /**
    * Request the stop info
    */
-  const fetchBusStop = async function () {
-    await stop.getStoppings()
-      .then(data => {
-        item = data;
-        self.broadcastItems();
+  const fetchBusStop = function () {
+    const stopWithoutName = stopID ? new kmb.Stop(stopID, null) : null;
+    stopWithoutName.getStoppings()
+      .then(data => data[0].stop.name)
+      .then(actualStopName => {
+        stopName = actualStopName;
+        const stop = new kmb.Stop(stopID, stopName);
+        stop.getStoppings()
+          .then(data => {
+            item = data;
+            self.broadcastItems();
+          })
       })
       .catch(error => {
+        Log.error(error);
         fetchFailedCallback(self, error);
       })
   };
@@ -94,7 +102,7 @@ const BusStopFetcher = function (stopID) {
   };
 
   this.stopName = function () {
-    return stop.name;
+    return stopName;
   }
 };
 
